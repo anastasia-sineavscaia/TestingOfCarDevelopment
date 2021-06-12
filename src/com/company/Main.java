4042483d5fd868fd3ec225a3ad11bfa6d07fbbb4
anastasia-sineavscaia <anastasia_sineavscaia@yahoo.com>
2021-06-12 12:11:58 -0500

    final red audi car at your service

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         car audi=new car();
+        audi.color="Red";
+        audi.model="111";
+        audi.door="3";
+        audi.model="A3";
+
     }
 
 
